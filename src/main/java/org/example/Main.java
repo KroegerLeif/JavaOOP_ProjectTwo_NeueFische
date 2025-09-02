b20b604e465ddd952a5793dbaf0040818040f468
KroegerLeif <Leif.kr@outlook.com>
2025-09-02 11:25:47 +0200

    Test of School

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,5 +12,12 @@ public class Main {
         students.add(new Student("Jane", "Doe", 2));
         students.add(new Student("John", "Smith", 3));
 
+        Schule schule = new Schule(students);
+        schule.getAllStudents();
+        schule.removeStudentById(1);
+        schule.getAllStudents();
+        schule.addStudent(new Student("Jane", "Smith", 4));
+        schule.getAllStudents();
+
     }
 }
