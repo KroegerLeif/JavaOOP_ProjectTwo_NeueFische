2959dd660e05788419c4ba5c38824ed728a049a3
KroegerLeif <Leif.kr@outlook.com>
2025-09-03 10:43:13 +0200

    Creation of Species and some changes to main. Now a owner is created

---
+++ src/main/java/org/example/zoo/Species.java
@@ -0,0 +1,4 @@
+package org.example.zoo;
+
+public record Species() {
+}
--- src/main/java/org/example/zoo/ZooMain.java
+++ src/main/java/org/example/zoo/ZooMain.java
@@ -2,10 +2,10 @@ package org.example.zoo;
 
 public class ZooMain {
     public static void main(String[] args) {
-        Animal animal1 = new Animal(1, "Pinguin", "Pinguin", 1);
-        Animal animal2 = new Animal(2, "Katze", "Katze", 2);
-        Animal animal3 = new Animal(3, "Hund", "Hund", 3);
-        Animal animal4 = new Animal(4, "Eule", "Eule", 4);
+        Animal animal1 = new Animal(1, "Pinguin", "Pinguin", 1,new Owner("John", 20, "Street 123"));
+        Animal animal2 = new Animal(2, "Katze", "Katze", 2,new Owner("Jane", 25, "Street 123"));
+        Animal animal3 = new Animal(3, "Hund", "Hund", 3,new Owner("Jane", 27, "Street 123"));
+        Animal animal4 = new Animal(4, "Eule", "Eule", 4,new Owner("Jane", 29, "Street 123"));
 
         System.out.println(animal1);
         System.out.println(animal1.equals(animal2));
